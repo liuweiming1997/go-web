455c3142ca62dc2681a945aa893d15169e17743e
vimi <vimiming@gmail.com>
2018-03-22 16:30:23 +0800

    [db] database Modules

---
+++ tools/db.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DBUSER=root
+DBHOST=95.163.202.160
+DBNAME=database-work
+DBPASSWORD=dc
+
+#get remote database sql to local
+function dump() {
+	mysqldump -h$DBHOST -u$DBUSER -p$DBPASSWORD $DBNAME > ./db/sql/latest_dump.sql
+}
+
+function restore() {
+	mysql -h$DBHOST -u$DBUSER -p$DBPASSWORD $DBNAME < ./db/sql/latest_dump.sql
+}
+
+case "$1" in
+	dump)
+		dump
+		;;
+
+	restore)
+		restore
+		;;
+
+	*)
+		echo "please choose one {dump | restore}"
+		exit 1
+esac
+
