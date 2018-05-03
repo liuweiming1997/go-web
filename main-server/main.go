ef5f25c67870c1897bff126d35518d09ea29e670
vimi <814865841@qq.com>
2018-05-03 15:17:04 +0800

    [bug] ignore main, change to main-server

---
+++ main-server/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/sundayfun/go-web/db/model"
+	"github.com/sundayfun/go-web/logs"
+	"github.com/sundayfun/go-web/services"
+	"github.com/sundayfun/go-web/util"
+)
+
+func root(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/savecompany", http.StatusFound)
+}
+
+type st struct {
+	Name []string
+}
+
+var s *st
+
+func test(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("../html/test.html")
+		log.Println(t.Execute(w, s))
+	} else {
+		r.ParseForm() //解析url传递的参数，对于POST则解析响应包的主体（request body）
+	}
+}
+
+func init() {
+	s = &st{}
+	s.Name = make([]string, 0, 4)
+	s.Name = append(s.Name, "1")
+	s.Name = append(s.Name, "2")
+	s.Name = append(s.Name, "3")
+	s.Name = append(s.Name, "4")
+	s.Name = append(s.Name, "5")
+
+	logs.LogToFile("a new start")
+	model.XOLog = func(s string, args ...interface{}) {
+		res := fmt.Sprintf("%s ---- %v\n", s, args)
+		logs.LogToFile(res)
+	}
+	fmt.Println(util.Command("pwd"))
+
+	http.HandleFunc("/", root)                                          //设置访问的路由
+	http.HandleFunc("/savecompany", services.SaveCompany)               //设置访问的路由
+	http.HandleFunc("/querysumforcompany", services.QuerySumForCompany) //设置访问的路由
+	http.HandleFunc("/querysumforself", services.QuerySumForSelf)       //设置访问的路由
+	http.HandleFunc("/test", test)                                      //设置访问的路由
+
+}
+
+func main() {
+	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ main-server/web-crawler.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/sundayfun/go-web/util/file"
+	"github.com/sundayfun/go-web/util/filter"
+)
+
+const (
+	SavePicturePath = "./resource/picture/"
+
+	// URL = "http://image.baidu.com/search/index?tn=baiduimage&ipn=r&ct=201326592&cl=2&lm=-1&st=-1&fm=result&fr=&sf=1&fmq=1525178045789_R&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&ie=utf-8&hs=2&word=baby"
+	// URL = "http://www.tooopen.com/img"
+	URL = "https://www.zhihu.com/question/66313867/answer/242883999"
+)
+
+func ImageFromImageUrl(imageUrl string, fileName string) error {
+	resp, err := http.Get(imageUrl)
+	defer resp.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(SavePicturePath + fileName)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	io.Copy(file, bytes.NewReader(body))
+	return nil
+}
+
+func ImageUrlFromHtml(html string) {
+	str := filter.ReImageURL.FindAllString(html, -1)
+	for i, val := range str {
+		fmt.Println(i, val)
+		err := ImageFromImageUrl(val, strconv.Itoa(i))
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func HtmlFromUrl(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func done() {
+	html, err := HtmlFromUrl(URL)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	ioutil.WriteFile("./resource/haha.html", []byte(html), 0664)
+	ImageUrlFromHtml(html)
+}
+
+func main() {
+	err := file.CreateDirIfNotExit(SavePicturePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	done()
+	// ImageFromImageUrl("http://img5.imgtn.bdimg.com/it/u=1011441765,2690175992&fm=27&gp=0.jpg", "vimi")
+	return
+}
