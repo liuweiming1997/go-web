a06353954eb4cdc9508688c763031949a809f395
liuweiming <liuweiming@bytedance.com>
2020-11-03 21:32:19 +0800

    add dzone

--- main-server/web-crawler.go
+++ main-server/web-crawler.go
@@ -3,8 +3,9 @@ package main
 import (
 	"runtime"
 	"time"
-	"github.com/sundayfun/go-web/main-server/servies/cnblogs"
 	"github.com/sundayfun/go-web/main-server/servies/cloudyun"
+	"github.com/sundayfun/go-web/main-server/servies/cnblogs"
+	"github.com/sundayfun/go-web/main-server/servies/dzone"
 	"github.com/sundayfun/go-web/main-server/servies/niuke"
 	"github.com/sundayfun/go-web/main-server/servies/niukemianjin"
 )
@@ -14,6 +15,7 @@ func main() {
 	go cloudyun.Producer()
 	go niuke.Producer()
   go niukemianjin.Producer()
+  go dzone.Producer()
 	for {
 		time.Sleep(time.Second * 30)
 		runtime.Gosched()
